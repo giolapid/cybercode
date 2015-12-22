3dc920016b62ea95d64b8275487ab2ec9c1f1a34
Gio Lapid <giolapid@gmail.com>
2015-12-21 16:33:50 -0800

    final with comments

--- count.sh
+++ count.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 #Gio Lapid
 #Counter App for bash
+
+#asks for name and reads it
 echo "Hello what is your name?"
 read name
 
-#sanitizing 
+#sanitizing the name and makes sure it's not empty
+#not a number or a symbols
 numbers='^[0-9]+$'
 while [[ -z "$name" ]] || [[ $name =~ $numbers ]] || [[ $name == *['!'@#\$%^\&*()_+]* ]]  ; do
   echo "Please use letters as your name"
   read name
 done
 
+#if the loop above passes the program will ask for a number
 echo "Hello $name, please choose a number between 5 and 100"
 read num
 
+#it will sanitize the number input and makes sure it's between 5 and 100
+#it will also make sure the input is not letters
 letter='[a-z]'
 while [[ $num =~ $letter ]] || [[ $num -lt '5' ]] || [[ $num -gt '100' ]] ; do
     echo "Please input number between 5-100. " 
     read num
 done
 
+#clear everything on the screen and do the count
 clear
 echo "$name, I am now counting to $num"
 
