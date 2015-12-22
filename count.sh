e0e95e101ba924783ee640e348a2a48ae44d7b33
Gio Lapid <giolapid@gmail.com>
2015-12-21 16:28:47 -0800

    final with while sanitations

--- count.sh
+++ count.sh
@@ -4,26 +4,26 @@
 echo "Hello what is your name?"
 read name
 
-echo  "Hello "$name
 #sanitizing 
-re='^[0-9]+$'
-if [[ $name =~ $re ]] ; then
-  echo "Please use letters as your name" >&2; exit 1
-fi
+numbers='^[0-9]+$'
+while [[ -z "$name" ]] || [[ $name =~ $numbers ]] || [[ $name == *['!'@#\$%^\&*()_+]* ]]  ; do
+  echo "Please use letters as your name"
+  read name
+done
 
-
-echo "Please choose a number between 5 and 100"
+echo "Hello $name, please choose a number between 5 and 100"
 read num
-re='^[0-9]+$'
-if ! [[ $num =~ $re ]] ; then
-  echo "Please use numbers" >&2; exit 1
-elif [ $num -lt 5 ] ; then
-  echo "Please choose a number greater than 5"
-elif [ $num -gt 100 ] ; then
-  echo "Please choose a number less than 100"
-else
-  for(( i=1; i < $num+1; i+=3));do
+
+letter='[a-z]'
+while [[ $num =~ $letter ]] || [[ $num -lt '5' ]] || [[ $num -gt '100' ]] ; do
+    echo "Please input number between 5-100. " 
+    read num
+done
+
+clear
+echo "$name, I am now counting to $num"
+
+for(( i=2; i < $num+1; i+=2)); do
     echo $i
-  done
-fi
+done
 
